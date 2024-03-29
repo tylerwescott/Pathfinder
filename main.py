7a3e43fbcd8aa243ac779437f0e1866993c68f34
Jonathan Phillips <jonp7802@gmail.com>
2024-03-29 17:33:00 -0400

    first commit of main.py for running the website

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = create_app()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':  # only runs website if main.py is used
+    app.run(debug=True)  # everytime a change is made in python code, it will automatically rerun the server
