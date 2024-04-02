c03e31fd54e4ad70f22e275685b156d3ed233887
ginaabascal <ginaabascal@gmail.com>
2024-04-02 04:53:00 -0400

    implemented database creation on visit to website

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy() #set up database
 DB_NAME = "database.db"
@@ -7,10 +8,23 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'TeamPathFinder'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
+    
     return app
+
+def create_database(app): #checks if db exists. if not, creates db
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print('Database created successfully')
